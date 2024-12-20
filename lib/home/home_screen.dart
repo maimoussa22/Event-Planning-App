d13783668742aa5507c7dc19e0c0589694acd941
Mai Moussa <maiimoussa2004@gmail.com>
2024-12-20 21:56:25 +0200

    Added initial point

---
+++ lib/home/home_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+  static const String routeName = 'homeScreen';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:event_planning_app/home/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main(){
@@ -8,7 +9,11 @@ class myApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      debugShowCheckedModeBanner: false,
+      initialRoute: HomeScreen.routeName,
+      routes: {
+        HomeScreen.routeName:(context) => HomeScreen(),
+      },
     );
   }
 
